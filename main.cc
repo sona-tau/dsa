72df773cc56151245558526f61f290f0aecdbf12
Sona Tau Estrada Rivera <sona@stau.space>
2026-03-15 22:52:29 -0400

    feat(bst): Added implementation for BST.

--- main.cc
+++ main.cc
@@ -1,91 +1,116 @@
 #include <iostream>
-using std::cout;
-using std::endl;
-using std::string;
+#include <memory>
+using namespace std;
 
-/// Empty list is null
+template <typename T>
+struct Node {
+	T data;
+	shared_ptr<Node<T>> left;
+	shared_ptr<Node<T>> right;
 
-/// Requirements:
-/// input: Φ [a, b, c, ...]
-/// output: [Φ, a, b, c, ...]
+	Node(T v) : data(v), left(nullptr), right(nullptr) {}
+};
 
-/// Requirements:
-/// input: [a, Φ, c, ...] Φ
-/// output: true
-///
-/// input: [a, b, c, ...] Φ
-/// output: false
+template <typename T>
+using Tree = shared_ptr<Node<T>>;
 
-/// Requirements:
-/// input: [a, b, c, ...] 2
-/// output: c
-///
-/// input: [a, b, c, ...] 0
-/// output: a
+template <typename T>
+Tree<T> insert(Tree<T> tree, T value) {
+	if (tree == nullptr)
+		return make_shared<Node<T>>(value);
 
-/// Requirements:
-/// input: [a, b, c] 0
-/// output: [b, c]
-///
-/// input: [a, b, c] 2
-/// output: [a, b]
-///
-/// input: [a, b, c] 10000
-/// output: [a, b, c]
+	if (value < tree->data) {
+		tree->left = insert(tree->left, value);
+		return tree;
+	} else {
+		tree->right = insert(tree->right, value);
+		return tree;
+	}
+}
+
+template <typename T>
+bool search(Tree<T> tree, T value) {
+	if (tree == nullptr)
+		return false;
+	if (tree->data == value)
+		return true;
+
+	if (value < tree->data)
+		return search(tree->left, value);
+
+	return search(tree->right, value);
+}
+
+template <typename T>
+Tree<T> remove(Tree<T> tree, T value) {
+	if (tree == nullptr)
+		return nullptr;
+
+	if (value < tree->data)
+		tree->left = remove(tree->left, value);
+	else if (value > tree->data)
+		tree->right = remove(tree->right, value);
+	else if (value == tree->data) {
+		if (tree->right == nullptr)
+			return tree->left;
+		if (tree->left == nullptr)
+			return tree->right;
+
+		tree->data = inorder_successor(tree->right);
+		tree->right = remove_inorder_successor(tree->right);
+	}
+
+	return tree;
+}
+
+template <typename T>
+Tree<T> remove_inorder_successor(Tree<T> tree) {
+	if (tree->left == nullptr)
+		return tree->right;
+
+	tree->left = remove_inorder_successor(tree->left);
+	return tree;
+}
+
+template <typename T>
+T inorder_successor(Tree<T> tree) {
+	if (tree->left == nullptr)
+		return tree->data;
+
+	return inorder_successor(tree->left);
+}
 
 // Helper function for printing the entire list
+template<typename T>
+void inorder(Tree<T> t) {
+	if (!t) return;
+	inorder(t->left);
+	cout << t->data << " ";
+	inorder(t->right);
+}
 
 int main() {
-	/*
-	List<int> list = init<int>();
+	Tree<int> tree = nullptr;
 
-	// build list: [1 .. 13]
-	for (int i = 1; i < 13; ++i) list = prepend(i, list);
-
-	cout << "Initial list: ";
-	print(list);
-
-	// indexing
-	int* m = index(list, 0);
-	cout << "index 0: ";
-	if (m != nullptr) cout << *m;
-	else cout << "not found";
+	tree = insert(tree, 5);
+	tree = insert(tree, 3);
+	tree = insert(tree, 7);
+	tree = insert(tree, 2);
+	tree = insert(tree, 4);
+	tree = insert(tree, 6);
+	tree = insert(tree, 8);
+	cout << "tree: ";
+	inorder(tree);
 	cout << endl;
 
-	m = index(list, 100);
-	cout << "index 100: ";
-	if (m != nullptr) cout << *m;
-	else cout << "not found";
+	cout << "Search 4: " << search(tree, 4) << endl;
+	cout << "Search 10: " << search(tree, 10) << endl;
+
+	tree = remove(tree, 7);
+
+	cout << "Search 7 after removal: " << search(tree, 7) << endl;
+
+	cout << "tree: ";
+	inorder(tree);
 	cout << endl;
-
-	m = index(list, 12);
-	cout << "index 12: ";
-	if (m != nullptr) cout << *m;
-	else cout << "not found";
-	cout << endl;
-
-	// contains
-	cout << "contains 2: " << (contains(list, 2) ? "true" : "false") << endl;
-	cout << "contains 5: " << (contains(list, 5) ? "true" : "false") << endl;
-	cout << "contains 13: " << (contains(list, 13) ? "true" : "false") << endl;
-
-	// removal
-	list = remove_at(list, 2);
-	cout << "After removing at index 2: ";
-	print(list);
-
-	list = remove_at(list, 0);
-	cout << "After removing at index 0: ";
-	print(list);
-
-	list = remove_at(list, 10);
-	cout << "After removing at index 10: ";
-	print(list);
-
-	list = remove_at(list, 100);
-	cout << "After removing at index 100: ";
-	print(list);
-
-	deinit(list);
-	*/
 }
